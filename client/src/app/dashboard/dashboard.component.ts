6f745a029c03a66de2ca4ba76b823f61534575de
roouugeer <rogeresteve5@hotmail.com>
2024-05-23 16:51:07 +0200

    implement widget

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { WidgetComponent } from './components/widget/widget.component';
 
 @Component({
   selector: 'acme-dashboard',
   standalone: true,
-  imports: [],
+  imports: [WidgetComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
