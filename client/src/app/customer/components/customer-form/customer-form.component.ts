8f2a727342c9327f8fd044fd27cce18642a5bf8d
Daniel Cueto <dcueto14.info@gmail.com>
2024-05-23 20:28:47 +0200

    feature: implemented product logic & data to customer & order

--- client/src/app/customer/components/customer-form/customer-form.component.ts
+++ client/src/app/customer/components/customer-form/customer-form.component.ts
@@ -1,12 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, Input, OnInit, Signal } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CustomerService } from '../../../shared/services/customer.service';
+import { Customer } from '../../../shared/interfaces/customer';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'acme-customer-form',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule, 
+    FormsModule, 
+    MatInputModule, 
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatButtonModule, 
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatIconModule
+  ],
   templateUrl: './customer-form.component.html',
   styleUrl: './customer-form.component.scss'
 })
-export class CustomerFormComponent {
+export class CustomerFormComponent implements OnInit {
+  private formBuilder = inject( FormBuilder );
+  private customerService = inject( CustomerService );
 
+  @Input() formType: 'new' | 'edit' = 'new';
+  @Input() customerToBeEdited!: Signal<Customer>;
+  @Input() submitButtonText = 'Add Product';
+
+  formData!: Customer;
+  customerForm = this.formBuilder.group({
+    companyName: ['', Validators.required],
+    contactName: ['', Validators.required],
+    phone: [0, Validators.required],
+    email: [''],
+    address: [''],
+  });
+
+
+  ngOnInit(): void {
+    // BUG THAT BLOCKS INIT FORM CORRECTLY
+    // this.updateFormValue();
+  }
+
+  constructor() {
+
+    effect( () => {
+      console.log( 'customer signal changed' );
+      this.updateFormValue();
+    })
+  }
+
+
+  errorMessage = '';
+
+  updateFormValue(){
+    if( this.formType === 'edit' ){
+
+      this.customerForm.setValue({
+        companyName: this.customerToBeEdited().companyName as string,
+        contactName: this.customerToBeEdited().contactName as string,
+        phone: this.customerToBeEdited().phone as number,
+        email: this.customerToBeEdited().email as string,
+        address: this.customerToBeEdited().address as string
+      });
+
+    }
+  }
+
+
+  handleFormSubmit(){
+
+    this.formData = {
+      companyName: this.customerForm.value.companyName as string,
+      contactName: this.customerForm.value.contactName as string,
+      phone: this.customerForm.value.phone as number,
+      email: this.customerForm.value.email as string,
+      address: this.customerForm.value.address as string,
+      orderIds: []
+    }
+
+    if( this.formType === 'new' ){
+      this.customerService.addCustomer( this.formData )
+        .subscribe( customer => console.log( customer , 'customer added'));
+
+      console.log( 'CUSTOMER CREATED' );
+      this.customerForm.reset();
+
+    } else if( this.formType === 'edit' ){
+      this.formData.orderIds = [ ...this.customerToBeEdited().orderIds as number[] ];
+      this.customerService.editCustomerById( this.customerToBeEdited().id as number, this.formData )
+        .subscribe( customer => console.log( customer, 'customer edited' ) );
+      console.log( 'CUSTOMER SAVED & EDITED' );
+    }
+
+  }
 }
--- client/src/app/order/pages/order-detail-page/order-detail-page.component.ts
+++ client/src/app/order/pages/order-detail-page/order-detail-page.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input, numberAttribute, signal } from '@angular/core';
+import { OrderService } from '../../../shared/services/order.service';
+import { OrderDetailsComponent } from '../../components/order-details/order-details.component';
+import { OrderFormComponent } from '../../components/order-form/order-form.component';
+import { Order } from '../../../shared/interfaces/order';
 
 @Component({
   selector: 'acme-order-detail-page',
   standalone: true,
-  imports: [],
+  imports: [OrderDetailsComponent, OrderFormComponent],
   templateUrl: './order-detail-page.component.html',
   styleUrl: './order-detail-page.component.scss'
 })
 export class OrderDetailPageComponent {
+  private orderService = inject( OrderService );
+  
+  @Input({transform: numberAttribute}) id = 0;
+  order = signal<Order>(
+    {
+      id: 0, 
+      orderDate: '', 
+      totalPrice: 0, 
+      customerId: 0, 
+      shippmentAddress: {
+        addressLine: '',
+        country: '',
+        city: '',
+        postalCode: '',
+        phone: ''
+      },
+      products: []
+    });
+
+  isOrderFormActive = this.orderService.isOrderFormActive;
+
+  ngOnInit(): void {
+    this.orderService.getOrderById( this.id ).subscribe( ( order ) => {
+      console.log( order );
+      this.order.set( order );
+    });
+  }
 
 }
